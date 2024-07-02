148a65e7b42c78c4342a2d823c4c52878d1aa2f5
bibarub <wi1717171713@gmail.com>
2024-07-02 21:28:27 +0300

    make the resulting iso a bit smaller by removing update data

--- pack-iso.sh
+++ pack-iso.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+for i in r11_iso_extracted/PSP_GAME/SYSDIR/UPDATE/*.* ; do rm $i; touch $i ; done
 
 mkisofs -U -xa -A "PSP GAME" -V "" -sysid "PSP GAME" -volset "" -p "" -publisher "" -o iso/r11-repacked.iso r11_iso_extracted/

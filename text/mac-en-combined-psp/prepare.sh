7b4cccd3e46ba48b58a92bc6fba9065445a3fd0c
bibarub <wi1717171713@gmail.com>
2024-07-03 04:46:53 +0300

    use the right encoding.

--- text/mac-en-combined-psp/prepare.sh
+++ text/mac-en-combined-psp/prepare.sh
@@ -2,12 +2,12 @@
 
 path="${0%/*}"
 
-rm -r $path/old 2>/dev/null
-mkdir $path/old
+rm -r $path/old $path/workdir 2>/dev/null
+mkdir $path/old $path/workdir
 
 for i in $path/utf8/*.txt; do sed -i -e 's|ç|c|g' -e 's|´|é|g' -e 's|`|à|g' -e 's|¨|ö|g' $i; done
-
-for i in $path/utf8/*.txt; do sed -i -e 's|é|▲|g' -e 's|à|▽|g' -e 's|ö|▼|g' $i; done
-for i in $path/utf8/*.txt; do iconv -f UTF-8 -t CP932 $i -o $path/`basename $i`; done
-cp $path/*.txt $path/old
-for i in $path/*.txt; do sed -i -e 's|\x81\xa3|\x87\xa7|g' -e 's|\x81\xa4|\x87\xa8|g' -e 's|\x81\xa5|\x87\xa9|g' $i; done
+cp $path/utf8/*.txt $path/workdir/
+for i in $path/workdir/*.txt; do sed -i -e 's|é|份|g' -e 's|à|仿|g' -e 's|ö|伃|g' $i; done
+cp $path/*.txt $path/old/
+for i in $path/workdir/*.txt; do iconv -f UTF-8 -t SHIFT_JISX0213 $i -o $path/`basename $i`; done
+rm -r $path/workdir
--- text/mac-en-combined-psp/reverse.sh
+++ text/mac-en-combined-psp/reverse.sh
@@ -7,9 +7,7 @@ mkdir $path/workdir $path/utf8
 
 cp $path/*.txt $path/workdir
 for i in $path/workdir/*.txt; do sed -i -e 's|\x87\xa7|\x81\xa3|g' -e 's|\x87\xa8|\x81\xa4|g' -e 's|\x87\xa9|\x81\xa5|g' $i; done
-for i in $path/workdir/*.txt; do iconv -f CP932 -t UTF-8 $i -o $path/utf8/`basename $i`; done
+for i in $path/workdir/*.txt; do iconv -f SHIFT_JISX0213 -t UTF-8 $i -o $path/utf8/`basename $i`; done
 for i in $path/utf8/*.txt; do sed -i -e 's|▲|é|g' -e 's|▽|à|g' -e 's|▼|ö|g' $i; done
-#for i in utf8/*.txt; do sed -i -e 's|c|ç|g' -e 's|é|´|g' -e 's|à|`|g' -e 's|ö|¨|g' $i; done
-#for i in utf8/*.txt; do iconv -f UTF-8 -t CP932 $i -o cp932/`basename $i`; done
 
 rm -r $path/workdir
